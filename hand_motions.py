1225b87c392c3964823bc9e42b204812cb452bea
Lucy Mo <lululucyyyyyyy@Lucys-MacBook-Pro.local>
2019-07-18 16:08:45 -0400

    it should work now :)

--- hand_motions.py
+++ hand_motions.py
@@ -11,35 +11,46 @@ from cnn_utils import *
 from PIL import Image
 import glob
 import random
-directions = ['up', 'down', 'left', 'right', 'zoom_in', 'zoom_out', 'two', 'three', 'four', 'five']
+from tensorflow.keras import layers
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+#directions = ['up', 'down', 'left', 'right', 'zoom_in', 'zoom_out', 'two', 'three', 'four', 'five']
 my_list = []
-for direction in directions:
-    path = "dataset/{}/*.JPG".format(direction)
-    temp = glob.glob(path)
-    for image in temp:
-        with open(image, 'rb') as file:
-            img = Image.open(file)
-            np_img = np.array(img)
-            my_list.append(np_img)
+path = "dataset/*.JPG"
+temp = glob.glob(path)
+for image in temp:
+    with open(image, 'rb') as file:
+        img = Image.open(file)
+        img = img.resize((224, 224))
+        np_img = np.array(img)
+        my_list.append(np_img)
 my_list = np.array(my_list)
-labels = [0,0,0,0,0,0,0,0,0,0,0,
+print(my_list.shape)
+# 0 up, 1 down, 2 left, 3 right, 4 zoom in, 5 zoom out, 6 two, 7 three, 8 four, 9 five
+labels = [0,0,0,0,0,0,0,0,0,0,0,0,
          1,1,1,1,1,1,1,1,1,1,1,
-         2,2,2,2,2,2,2,2,2,2,2,
-         3,3,3,3,3,3,3,3,3,3,3,
-         4,4,4,4,4,4,4,4,4,4,4,
-         5,5,5,5,5,5,5,5,5,5,5,
+         2,2,2,2,2,2,2,2,2,2,2,2,
+         3,3,3,3,3,3,3,3,3,3,3,3,
+         4,4,4,4,4,4,4,4,4,4,4,4,
+         5,5,5,5,5,5,5,5,5,5,5,5,
          6,6,6,6,6,6,6,6,6,6,6,
-         7,7,7,7,7,7,7,7,7,7,
-         8,8,8,8,8,8,8,8,8,8,8,
-         9,9,9,9,9,9,9,9,9,9,9]
+         7,7,7,7,7,7,7,7,7,7,7,
+         8,8,8,8,8,8,8,8,8,8,8,8,
+         9,9,9,9,9,9,9,9,9,9,9,9]
+print(len(labels))
 labels = np.array(labels)
 c = np.c_[my_list.reshape(len(my_list), -1), labels.reshape(len(labels), -1)]
 X_orig = c[:, :my_list.size//len(my_list)].reshape(my_list.shape)
 Y_orig = c[:, my_list.size//len(my_list):].reshape(labels.shape)
-X_train_orig = X_orig[0:10]
-Y_train_orig = Y_orig[0:10]
-X_test_orig = X_orig[11:109]
-Y_test_orig = Y_orig[11:109]
+X_test_orig = X_orig[0:10]
+Y_test_orig = Y_orig[0:10]
+X_train_orig = X_orig[11:117]
+Y_train_orig = Y_orig[11:117]
+print(len(X_train_orig))
+print(len(Y_train_orig))
+print(len(X_test_orig))
+print(len(Y_test_orig))
 X_train = X_train_orig/255
 X_test = X_test_orig/255
 Y_train = convert_to_one_hot(Y_train_orig, 10).T
@@ -70,17 +81,23 @@ def forward_prop(X, parameters):
     Z2 = tf.nn.conv2d(P1,W2, strides = [1,1,1,1], padding = 'SAME')
     A2 = tf.nn.relu(Z2)
     P2 = tf.nn.max_pool(A2, ksize = [1,4,4,1], strides = [1,4,4,1], padding = 'SAME')
-    P2 = tf.contrib.layers.flatten(P2)
-    Z3 = tf.contrib.layers.fully_connected(P2, 6, activation_fn=None)
+    P2 = tf.layers.Flatten()(P2)
+    Z3 = tf.keras.layers.Dense(10, activation=None)(P2)
     return Z3
 
 def compute_cost(Z3, Y):
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=Z3,labels=Y))
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=Z3,labels=Y))
+    #cost = -tf.reduce_sum(Y*tf.log(tf.clip_by_value(Z3,1e-10,1.0)))
+    #cost = -tf.reduce_sum(Y*tf.log(tf.clip_by_value(Z3,1e-10,1.0)))
+    #labels_sum = tf.reduce_sum(Y, axis=-1)
+    #print(labels)
+    #softmax = tf.nn.softmax(Z3)
+    #cost = tf.reduce_mean(-tf.reduce_sum(softmax * tf.log(Y), axis=-1))
     return cost
 
 def model(X_train, Y_train, X_test, Y_test, learning_rate = 0.009, num_epochs = 100, minibatch_size = 64, print_cost = True):
     ops.reset_default_graph()                         
-    #tf.set_random_seed(1)
+    tf.set_random_seed(1)
     seed = 3                                          
     (m, n_H0, n_W0, n_C0) = X_train.shape             
     n_y = Y_train.shape[1]                            
@@ -91,12 +108,12 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate = 0.009, num_epochs = 
     cost = compute_cost(Z3, Y)
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
     init = tf.global_variables_initializer()
-    
+
     with tf.Session() as sess:
         sess.run(init)
         
         for epoch in range(num_epochs):
-            minibatch_cost = 0.
+            minibatch_cost = 0
             num_minibatches = int(m / minibatch_size)
             seed = seed + 1
             minibatches = random_mini_batches(X_train, Y_train, minibatch_size, seed)
@@ -128,3 +145,6 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate = 0.009, num_epochs = 
 
         return train_accuracy, test_accuracy, parameters
 _, _, parameters = model(X_train, Y_train, X_test, Y_test)
+print(done)
+
+
