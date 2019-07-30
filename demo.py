4b4bf00e7077b8f3ad8029c01c7dc897ccd5eab4
Lucy Mo <lululucyyyyyyy@Lucys-MacBook-Pro-2.local>
2019-07-29 20:53:24 -0400

    final changes

--- demo.py
+++ demo.py
@@ -2,6 +2,8 @@ import subprocess
 from PIL import Image
 import numpy as np
 import tensorflow as tf
+import cv2
+import time
 
 interpreter = tf.lite.Interpreter(model_path='the_tflite.tflite')
 interpreter.allocate_tensors()
@@ -9,8 +11,17 @@ interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
+cam = cv2.VideoCapture(0)
+word = ''
+
+num = 0
 while True:
-	img = subprocess.call("imagesnap -q -w 1.00 img.jpeg", shell=True)
+	#display camera input
+	ret_val, image = cam.read()
+	image = cv2.flip(image, 1)
+	
+	#take image and process
+	img = subprocess.call("imagesnap -q -w 0.01 img.jpeg", shell=True)
 	img = Image.open("img.jpeg")
 	img = img.resize((224, 224))
 
@@ -20,11 +31,23 @@ while True:
 
 	interpreter.set_tensor(input_details[0]['index'], input_data) 
 
+	toc = time.time()
 	interpreter.invoke()
+	tic = time.time()
+	the_time = tic - toc
 
 	output_data = interpreter.get_tensor(output_details[0]['index'])
 	output = np.argmax(output_data)
 	if output == 1:
-		print('one')
+		print('one', the_time)
+		word = 'one'
 	else:
-		print('five')
+		print('five', the_time)
+		word = 'five'
+
+	#show camera input
+	cv2.imshow('my webcam', image)
+	if cv2.waitKey(1) == 27: 
+		break  # esc to quit
+
+cv2.destroyAllWindows()
